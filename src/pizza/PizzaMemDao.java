08800bed91c719349ff059a23505f50d493e6888
Champetier <todave11@gmail.com>
2018-10-15 11:50:45 +0200

    ajoute d'une méthode findIndexPizzaByCode et simplification des autres méthodes par celle-ci + commentaire

--- src/pizza/PizzaMemDao.java
+++ src/pizza/PizzaMemDao.java
@@ -10,9 +10,9 @@ import iPizza.IPizzaDao;
 
 public class PizzaMemDao implements IPizzaDao {
 
-	private ArrayList<Pizza> listPizza = null;
+	private ArrayList<Pizza> listPizza = null; //Liste D'objet Pizza
 
-	public PizzaMemDao() {
+	public PizzaMemDao() { //Constructeur avec 3 pizza par default
 		super();
 
 		this.listPizza = new ArrayList<Pizza>();
@@ -24,66 +24,70 @@ public class PizzaMemDao implements IPizzaDao {
 	}
 
 	@Override
-	public ArrayList<Pizza> findAllPizzas() {
+	public ArrayList<Pizza> findAllPizzas() {	//retourn la liste de pizza
 		return getListPizza();
 	}
 
 	@Override
-	public String toString() {
+	public String toString() {			//affiche list de pizza
 		return "PizzaMemDao [listPizza=" + listPizza + "]";
 	}
 
-	public ArrayList<Pizza> getListPizza() {
+	public ArrayList<Pizza> getListPizza() {		//getter
 		return listPizza;
 	}
 
-	public void setListPizza(ArrayList<Pizza> listPizza) {
+	public void setListPizza(ArrayList<Pizza> listPizza) {//setter
 		this.listPizza = listPizza;
 	}
 
 	@Override
-	public void saveNewPizza(Pizza pizza) {
+	public void saveNewPizza(Pizza pizza) {			//ajoute pizza à la liste 
 		listPizza.add(pizza);
 
 	}
 
 	@Override
-	public void deletePizza(String codePizza) {
-		for (int i = 0; i < listPizza.size(); i++) {
-			if (listPizza.get(i).getCode().equals(codePizza)) {
-				listPizza.remove(listPizza.get(i));
-			}
-		}
-
+	public void deletePizza(String codePizza) {		//supprime la pizza de code codePizza
+		int index=findIndexPizzaByCode(codePizza);
+		if (index!=-1) {
+			listPizza.remove(index);
+		}	
 	}
 
 	@Override
-	public void updatePizza(String codePizza, Pizza pizza) {
-		for (int i = 0; i < listPizza.size(); i++) {
-			if (listPizza.get(i).getCode().equals(codePizza)) {
-				listPizza.set(i, pizza);
-				System.out.println("Pizza chager \n\n");
-			}
+	public void updatePizza(String codePizza, Pizza pizza) {	//met à jour la pizza de code codePizza
+		int index=findIndexPizzaByCode(codePizza);
+		if (index!=-1) {
+			listPizza.set(index, pizza);
+			System.out.println("Pizza changer \n\n");
 		}
-
 	}
 
 	@Override
-	public Pizza findPizzaByCode(String codePizza) {
+	public Pizza findPizzaByCode(String codePizza) {			//renvoi la pizza de code codePizza
 		Pizza res = null;
+		int index=findIndexPizzaByCode(codePizza);
+		if (index!=-1) {
+			res = listPizza.get(index);
+		}
+		return res;
+	}
+	
 
+	public int findIndexPizzaByCode(String codePizza) {			//renvoi la position du codePizza dans la liste
+		int res = -1;
 		for (int i = 0; i < listPizza.size(); i++) {
 			if (listPizza.get(i).getCode().equals(codePizza)) {
-				res = listPizza.get(i);
+				res = i;
 				break;
 			}
 		}
-
 		return res;
 	}
 
 	@Override
-	public boolean isPizzaExists(String codePizza) {
+	public boolean isPizzaExists(String codePizza) {			//renvoi true si la pizza existe sinon false
 		boolean res = false;
 		if (this.findPizzaByCode(codePizza) != null) {
 			res = true;
