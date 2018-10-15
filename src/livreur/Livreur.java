a6bc13f6e07b5fb1a2d4538170adacc1419b0ee6
champetiert <ChampetierT@DESKTOP-K0FKSE8.home>
2018-10-15 22:06:32 +0200

    waw

--- src/livreur/Livreur.java
+++ src/livreur/Livreur.java
@@ -4,6 +4,11 @@ import iPizza.ICompteStat;
 
 public class Livreur implements ICompteStat {
 
+	private int id;
+	private String nom;
+	private String prenom;
+	private double solde;
+	
 	@Override
 	public double getSolde() {
 		// TODO Auto-generated method stub
